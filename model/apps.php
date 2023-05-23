07b57d97608fbdb0340b3e622a5836ebe7f42ca4
Andrei Stezenko <andrew.stezenko@gmail.com>
2023-05-24 02:18:46 +0300

    upd: re-write methods using fopen().

--- model/apps.php
+++ model/apps.php
@@ -2,8 +2,12 @@
 
 function getApps(): ?array
 {
-	if( ! $lines = file(  'db/apps.txt' ) ) return null;
+	if( ! $file = fopen( 'db/apps.txt', 'r' ) ) return null;
 
+	$content = fread( $file, filesize( 'db/apps.txt' ) );
+	fclose( $file );
+	$lines = explode( "\n", $content );
+	unset( $lines[count( $lines ) - 1] );	// Delete an empty element with line break symbol.
 	$apps = [];
 
 	foreach( $lines as $line ) $apps[] = strLineToArr( $line );
@@ -13,8 +17,7 @@ function getApps(): ?array
 
 function strLineToArr( string $line ): array
 {
-	$line	= rtrim( $line );
-	$parts	= explode( ';', $line );
+	$parts = explode( ';', $line );
 
 	return ['date' => $parts[0], 'name' => $parts[1], 'phone' => $parts[2]];
 }
@@ -24,7 +27,12 @@ function addApp( string $name, string $phone ): bool
 	$date	= date( 'Y-d-m H:i:s' );
 	$app	= "$date;$name;$phone\n";
 
-	if( ! file_put_contents( 'db/apps.txt', $app, FILE_APPEND ) ) return false;
+	if( ! $file = fopen( 'db/apps.txt', 'a' ) ) return false;
+
+	$bytes = fwrite( $file, $app );
+	fclose( $file );
+
+	if( ! $bytes ) return false;
 
 	return true;
 }
